d5e12baabe36e6217977576ab448f046e142bd9f
Yanis <yanis@gmail.com>
2026-02-05 14:29:55 +0100

    Correction du projet ludotheque

---
+++ src/main/java/fr/eni/ludotheque/bo/Jeu.java
@@ -0,0 +1,49 @@
+package fr.eni.ludotheque.bo;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name="JEUX")
+public class Jeu {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="no_jeu")
+	@EqualsAndHashCode.Exclude
+	private Integer noJeu;
+	
+	@Column( length=50, nullable=false)
+	@NonNull
+	private String titre;
+	
+	@Column(length=13, nullable=false, unique=true)
+	@NonNull private String reference;
+	
+	@Column(nullable=true)
+	private int ageMin;
+	
+	@Column( nullable=true)
+	private String description;
+
+	private int duree;
+	
+	@Column(nullable=false)
+	@NonNull
+	private Float tarifJour;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "JEUX_GENRES", 
+		joinColumns = @JoinColumn(name="no_jeu"),
+		inverseJoinColumns = @JoinColumn(name="no_genre"))
+	private List<Genre> genres = new ArrayList<>();
+	
+	public void addGenre(Genre g) {
+		genres.add(g);
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into genres (no_genre, libelle) values (1, 'Jeu de plateau');
+insert into genres (no_genre, libelle) values (2, 'Jeu de cartes');
+insert into genres (no_genre, libelle) values (3, 'Jeu de stratégie');
+insert into genres (no_genre, libelle) values (4, 'Coopératif');
+insert into genres (no_genre, libelle) values (5, 'Jeu de dé');
+insert into genres (no_genre, libelle) values (6, 'Jeu d''enquete');
+delete from jeux;
